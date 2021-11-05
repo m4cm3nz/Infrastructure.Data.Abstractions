253e8faf8fba21d7a3200514627c2c8db826b241
RAFAEL MACMENZ <rafael@prestadores.refere.com.br>
2021-11-05 13:58:48 -0300

    feat: Added an interface to dowload content BinaryData

--- Infrastructure.Data.Abstractions/IBlobStorage.cs
+++ Infrastructure.Data.Abstractions/IBlobStorage.cs
@@ -1,8 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data.Abstractions
 {
+    public interface IDownloadContent
+    {
+        Task<BinaryData> DownloadContent(string fileName);
+    }
+
     public interface IBlobStorage
     {
         Task Upload<T>(T content, string fileName);
